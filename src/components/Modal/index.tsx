04f2b00ed2cd681dff7a71d209f1cf1af5cd015e
Proac-Tee <babatundetaiwo038@gmail.com>
2025-08-05 06:02:00 +0100

    feat(modal): modal component improved

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,6 +4,8 @@ import {
 	useRef,
 	type ReactNode,
 	type ForwardedRef,
+	type MouseEvent,
+	type KeyboardEvent,
 } from "react";
 import { useDialog } from "./hooks/useDialog";
 import { cn } from "@/lib/utils";
@@ -24,52 +26,47 @@ const Modal = forwardRef(function Modal(
 	{ children, className, showCloseIcon = true, closeIcon }: ModalProps,
 	ref: ForwardedRef<ModalHandle>,
 ) {
-	// internal ref for the dialog
 	const dialogRef = useRef<HTMLDialogElement | null>(null);
-
 	const { toggle } = useDialog({ dialogRef });
 
-	// expose toggle method via ref
-	useImperativeHandle(ref, () => ({
-		toggle,
-	}));
+	useImperativeHandle(ref, () => ({ toggle }), [toggle]);
+
+	const handleBackdropClick = (event: MouseEvent<HTMLDialogElement>) => {
+		if (event.currentTarget === event.target) toggle();
+	};
+
+	const handleEscapeKey = (event: KeyboardEvent<HTMLDialogElement>) => {
+		if (event.key === "Escape") toggle();
+	};
 
 	return (
 		<dialog
 			ref={dialogRef}
-			onClick={(event) => {
-				if (event.currentTarget === event.target) {
-					toggle();
-				}
-			}}
-			onKeyDown={(event) => {
-				if (event.key === "Escape") {
-					toggle();
-				}
-			}}
-			className={cn("")}
+			onClick={handleBackdropClick}
+			onKeyDown={handleEscapeKey}
+			className=""
 		>
 			<div
 				className={cn(
-					"relative transition-all bg-white max-w-[90vw] max-h-[90vh] rounded-[2.5rem] p-4",
+					"relative max-w-[90vw] max-h-[90vh] p-4 rounded-[2.5rem] bg-white transition-all",
 					className,
 				)}
 			>
 				{children}
-			</div>
 
-			{showCloseIcon && (
-				<button
-					type="button"
-					className={cn(
-						"absolute top-2 right-2 bg-ui-white rounded-full p-1 flex justify-center items-center hover:bg-ui-primary hover:text-ui-white transition-all shadow-md ease-in-out duration-300 cursor-pointer",
-					)}
-					onClick={toggle}
-				>
-					{closeIcon ?? <Cancel />}
-					<span className="sr-only">Close Modal</span>
-				</button>
-			)}
+				{showCloseIcon && (
+					<button
+						type="button"
+						onClick={toggle}
+						className={cn(
+							"cursor-pointer absolute top-2 right-2 p-1 rounded-full bg-white shadow-md hover:bg-ui-primary hover:text-white transition ease-in-out duration-300 flex items-center justify-center",
+						)}
+						aria-label="Close Modal"
+					>
+						{closeIcon ?? <Cancel />}
+					</button>
+				)}
+			</div>
 		</dialog>
 	);
 });
